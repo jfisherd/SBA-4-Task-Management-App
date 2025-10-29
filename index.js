07cffb8c5ab74d9ee057f5b5fa0d4537f115d57b
Josh Fisher <jfisherd@gmail.com>
2025-10-29 10:23:54 -0400

    tasks are now handled as objects, addTask and addTaskButton functions updated to reflect

--- index.js
+++ index.js
@@ -1,10 +1,10 @@
-myTasks = []
+myTasks = [] // array containing the task objects
 
 function addTask(task) {
-    if (shoppingList.includes(task)) {
-        return alert(`${task} is already a task!`)
+    if (myTasks.includes(task.name)) { // .name added to check only name of task
+        return alert(`${task.name} is already a task!`)
     } else {
-        myTasks.push(task)
+        myTasks.push(task) //add task object to the myTasks array
     }
 }
 
@@ -13,10 +13,41 @@ function filterTasks(searchTerm) {
 }
 
 
-taskInput = document.getElementById("taskInput");
+taskName = document.getElementById("taskName");
+taskCategory = document.getElementById("taskCategory");
+taskDeadline = document.getElementById("taskDeadline");
+taskStatus = document.getElementById("taskStatus");
 addTaskButton = document.getElementById("addTaskButton");
-removeTaskButton = document.getElementById("removeTaskButton");
 taskList = document.getElementById("taskList");
+
+function updateTasks() {
+    taskList.innerHTML = ""
+    for (i=0;i<myTasks.length;i++) {
+        listTask = document.createElement("li")
+        listTask.innerText = myTasks[i]
+        taskList.appendChild(listTask)
+    }
+}
+
+addTaskButton.addEventListener("click", function () {
+  taskObject = {}
+  taskObject.name = taskName
+  taskObject.category = taskCategory
+  taskObject.deadline = taskDeadline
+  taskObject.status = taskStatus
+ 
+  if (taskName === "" || taskCategory === "" || taskDeadline === "" || taskStatus=== "") {
+    alert("Please enter all fields before adding a task!")
+    return
+  }
+  addItem(taskObject)
+  updateCart()
+  itemInput.value = "" 
+})
+
+
+
+
 
 
 
